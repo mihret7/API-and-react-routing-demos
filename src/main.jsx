962e00729ef677fc27062d8cd6ec2a090fc8c95a
Mihret7 <mihretbizuayehu777@gmail.com>
2025-05-13 20:47:27 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-// bootstrap css & js
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+// Bootstrap CSS & JS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/API-and-react-routing-demos/">
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter basename="/API-and-react-routing-demos">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
