32aed820b85a66a10da1d53ed289cee414a8c959
Mihret7 <mihretbizuayehu777@gmail.com>
2025-05-13 18:04:45 +0300

    last update

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/API-and-react-routing-demos/">
     <App />
   </BrowserRouter>
 );
